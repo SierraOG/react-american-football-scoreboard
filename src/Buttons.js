42c39598495010126b7ae69b12d018d2f4c5f1c0
SierraOG <sierrao@utexas.edu>
2019-07-15 17:38:07 -0500

    added handler function

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./App.css";
 
 const TopRow = (countObj) => {
+    function incrementTeam(team, amount){
+        countObj.counts[team + 'Counter'](countObj.counts[team + 'Count'] + amount)
+    }
     return (
         <section className="buttons">
             <div className="homeButtons">
             {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-            <button onClick={() => countObj.counts.homeCounter(countObj.counts.homeCount + 7)} className="homeButtons__touchdown">Home Touchdown</button>
-            <button onClick={() => countObj.counts.homeCounter(countObj.counts.homeCount + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+            <button onClick={() => incrementTeam('home', 7)} className="homeButtons__touchdown">Home Touchdown</button>
+            <button onClick={() => incrementTeam('home', 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
             </div>
             <div className="awayButtons">
-            <button onClick={() => countObj.counts.awayCounter(countObj.counts.awayCount + 7)} className="awayButtons__touchdown">Away Touchdown</button>
-            <button onClick={() => countObj.counts.awayCounter(countObj.counts.awayCount + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+            <button onClick={() => incrementTeam('away', 7)} className="awayButtons__touchdown">Away Touchdown</button>
+            <button onClick={() => incrementTeam('away', 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
             </div>
         </section>
     )
